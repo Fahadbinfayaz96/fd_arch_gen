9a918ad024f928b5d60326507ad18326d237e81d
fahadbinfayaz96 <fahadbinfayaz100@gmail.com>
2026-05-01 14:58:35 +0530

    fix: use Isolate.resolvePackageUri for reliable template path resolution

--- lib/commands/feature.dart
+++ lib/commands/feature.dart
@@ -2,7 +2,7 @@ import 'package:args/args.dart';
 import '../internals/config_loader.dart';
 import '../generators/feature_generator.dart';
 
-void featureCommand(String name, ArgResults flags) {
+void featureCommand(String name, ArgResults flags) async {
   final baseConfig = loadConfig();
   final config = mergeFlags(baseConfig, flags);
   generateFeature(name, config);
